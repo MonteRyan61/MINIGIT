90de0ff80742622bfe464cc3504705a91264678a
MonteRyan61 <ryan.monteleone@colorado.edu>
2021-04-15 19:57:23 -0600

    Updated to correct file names according to write up April 15 7:57

---
+++ miniGit.cpp
@@ -0,0 +1,30 @@
+#include "miniGit.hpp"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void git::addFile(string filename)
+{
+    ifstream inFile;
+    inFile.open(filename);
+    //if (inFile.error()) //if it doesn't open
+    {
+        cout << "file failed to open" << endl;
+    }
+}
+
+bool git::removeFile(string filename)
+{
+    
+}
+
+void git::commitChanges()
+{
+// cout << headCommit->commitNumber << endl;
+}
+
+void checkout(int _commitNumber)
+{
+    
+}
